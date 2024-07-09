40be4f841444938aeeccee65eade830f7a1e1033
Gurpreet Kaur Mahun <priti.kaur888@gmail.com>
2024-07-09 22:34:56 +0100

    branch main

--- obj/Debug/net8.0/FinalYearProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalYearProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c160ee7fa11fbaa79fcdfba4dc2dd081541a29d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
